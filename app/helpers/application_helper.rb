63cbe464e5eb48fc2d4966a038eb5eaf2eff41d8
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-05-14 23:17:36 +0200

    Added the link_to_nav helper method to the ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def link_to_nav(name, path, icon, count = nil, html_options = nil)
+    content_tag "li", menu_item_options(path) do
+      link_to(path, html_options) do
+        concat content_tag(:span, nil, class: "glyphicon glyphicon-#{icon}")
+        concat " #{name}"
+        concat content_tag(:span, count, class: "badge pull-right") if count
+      end
+    end
+  end
+
+  private
+
+  def menu_item_options(path)
+    if current_page?(path)
+      { class: "active" }
+    else
+      {}
+    end
+  end
+
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+
+  describe "#link_to_nav" do
+    let(:li) { helper.link_to_nav(:pages, pages_path, :file, 26) }
+
+    it "returns an list-item with the specified settings" do
+      expect(li).to eq '<li><a href="/pages"><span class="glyphicon glyphicon-file"></span> pages<span class="badge pull-right">26</span></a></li>'
+    end
+  end
+
+end
